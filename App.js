3129ae0fc30d468899eeda31a94079c8e7cc4967
Satish Phogat <satish.phogat@kelltontech.com>
2020-05-31 10:36:22 +0530

    react navigation installation

--- App.js
+++ App.js
@@ -3,12 +3,17 @@ import {Text, View} from 'react-native'
 import Parent from './Parent.js';
 import Login from './src/Login.js';
 import ForgotPassword from './src/ForgotPassword.js';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default class App extends Component {
   render() {
     return (
       // <Login></Login>
-      <ForgotPassword></ForgotPassword>
+      <NavigationContainer>
+              <ForgotPassword></ForgotPassword>
+      </NavigationContainer>
     )
   }
 }
