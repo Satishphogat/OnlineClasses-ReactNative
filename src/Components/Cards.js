d8eed3279ebc87ddead60fc7289f7746b4939343
Satish Phogat <satish.phogat@kelltontech.com>
2020-06-20 21:19:33 +0530

    refactor: home: ui.

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,51 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+import Constant from '../Utility/Constant.js'
 
 const DATA = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
     title: 'First Item',
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
     title: 'Second Item',
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
     title: 'Third Item',
   },
 ];
 
-function Item({ title }) {
-  return (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
-}
+class Cards extends Component {
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={({ item }) => <Item title={item.title} />}
-        keyExtractor={item => item.id}
-      />
-    </SafeAreaView>
-  );
-}
+  render() {
+    return (
+      <FlatList 
+      style={{height: 200}}
+      data = {DATA}
+      horizontal={true}
+      renderItem={({item}) => 
+      <Text style={styles.card}>{item.title}</Text>
+    }></FlatList>
+    )}}
+
+    export default Cards;
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 10,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
+  card: {
+    backgroundColor: CustomColor.lightGrayThemeColor,
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
+    marginHorizontal: 8,
+    width: 300,
+    borderRadius: 5,
+    overflow: 'hidden'
   },
   title: {
     fontSize: 32,
--- src/ViewControllers/Tabbar/Home.js
+++ src/ViewControllers/Tabbar/Home.js
@@ -4,122 +4,98 @@ import { SectionList, View, StyleSheet, Text, SafeAreaView, Image, TouchableHigh
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import CustomColor from '../../Utility/Constant.js';//'../../Utility/Constant.js';
-import CustomCard from '../../Components/CustomCard.js';
+import Cards from '../../Components/Cards.js';
 
-const sectionData = [
-    {
-      title: "Main dishes",
-      data: ["Pizza", "Burger", "Risotto"]
-    },
-    {
-      title: "Sides",
-      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
-    },
-    {
-      title: "Drinks",
-      data: ["Water", "Coke", "Beer"]
-    },
-    {
-      title: "Desserts",
-      data: ["Cheese Cake", "Ice Cream"]
-    }
-  ];
-
-//   const DATA = [
-//     {
-//       title: 'First Item',
-//     },
-//     {
-//       title: 'Second Item',
-//     },
-//     {
-//       title: 'Third Item',
-//     },
-//   ];
-  
-  function Item({ title }) {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
-    );
-  }
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
 
 class Home extends Component {
 
-    onPressLiveImage = () =>{
-        alert('success')
-    }
+  onPressLiveImage = () => {
+    alert('success')
+  }
 
-    liveClassView = () => {
-        return <View style={styles.liveClass} >
-            {/* <Image source={LocalImages.liveImage} /> */}
-            <TouchableHighlight onPress={this.onPressLiveImage}>
-                <Text style = {{color: CustomColor.white, fontSize: 24, fontWeight: '800'}}>LIVE CLASSES
+  liveClassView = () => {
+    return <View style={styles.liveClass} >
+      {/* <Image source={LocalImages.liveImage} /> */}
+      <TouchableHighlight onPress={this.onPressLiveImage}>
+        <Text style={{ color: CustomColor.white, fontSize: 24, fontWeight: '800' }}>LIVE CLASSES
                 </Text>
-            </TouchableHighlight>
-        </View>
-      };
+      </TouchableHighlight>
+    </View>
+  };
 
-    render() {
-        return (
-            <SafeAreaView>
+  render() {
+    return (
+      <SafeAreaView>
 
-                <SectionList sections={sectionData}
-                    renderItem={({ item }) => 
-                     <CustomCard></CustomCard>}></SectionList>
-                     {/* <FlatList
-                //       data={sectionData}
-                //       horizontal={true}
-                //       renderItem={({ item }) => <Item title={item.data} />}
-                //     />
-                // }
-                //      renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-                //      ListHeaderComponent={this.liveClassView}
-                // >
-                // </SectionList> */}
-            </SafeAreaView>
-        )
-    }
+        <SectionList
+        style = {{top: 50}}
+          sections={[
+            { title: 'Upcomming classes', data: ['ALTERED'] },
+            { title: 'Recent classes', data: ['BEST MEN'] },
+          ]} 
+          renderItem={({ item }) => 
+          <Cards></Cards>
+        }
+          renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          // keyExtractor={(item, index) => index}
+        />
+      </SafeAreaView>
+    )
+  }
 }
 
 export default Home;
 
 const styles = StyleSheet.create({
-    liveClass: {
-        height: 270,
-        backgroundColor: 'green',
-        justifyContent: 'center',
-        alignItems:'center'
-    },
-    container: {
-        flex: 1,
-        paddingTop: 22
-    },
-    sectionHeader: {
-        padding: 10,
-        paddingHorizontal: 16,
-        fontSize: 18,
-        fontWeight: '600',
-        height: 44,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: CustomColor.lightGrayThemeColor
-    },
-    // item: {
-    //     padding: 10,
-    //     fontSize: 18,
-    //     height: 44,
+  liveClass: {
+    height: 270,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  sectionHeader: {
+    padding: 10,
+    paddingHorizontal: 16,
+    fontSize: 18,
+    fontWeight: '600',
+    height: 44,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: CustomColor.lightGrayThemeColor
+  },
+  // item: {
+  //     padding: 10,
+  //     fontSize: 18,
+  //     height: 44,
 
-    // },
-      item: {
-        backgroundColor: '#f9c2ff',
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-        height: 100,
-      },
-      title: {
-        fontSize: 32,
-      },
+  // },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    height: 100,
+  },
+  title: {
+    fontSize: 32,
+  },
+  card: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 8,
+    width: 300,
+    borderRadius: 5,
+  },
 })
