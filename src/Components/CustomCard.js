3d05ac70656c60d228206446497e1b88d83eccd9
Satish Phogat <satish.phogat@kelltontech.com>
2020-06-20 16:29:01 +0530

    temporary commit.

--- src/Components/CustomCard.js
+++ src/Components/CustomCard.js
@@ -1,33 +1,155 @@
-import React, {Component} from 'react'
-import {View, StyleSheet, Image} from 'react-native';
-import CustomColor from '../Utility/Constant.js';
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
 
-const CustomCard = () => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.card}>
-                {/* <Image source={require('')}></Image> */}
-            </View>
-        </View>
-    )
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
 }
 
-export default CustomCard;
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: CustomColor.lightGrayThemeColor, 
-        height: 170, 
-        flexDirection: 'row'
-    },
-    card: {
-        backgroundColor: CustomColor.white, 
-        flex: 0.7, 
-        borderRadius: 5, 
-        shadowColor: CustomColor.lightGray, 
-        shadowOpacity: 1.0, 
-        marginHorizontal: 16, 
-        marginVertical: 5,
-        shadowOffset: { width: 0, height: 0 },
-    }
-})
+  container: {
+    flex: 1,
+    marginTop: 10,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {Component} from 'react'
+// import {View, StyleSheet, Image, FlatList, Text} from 'react-native';
+// import CustomColor from '../Utility/Constant.js';
+
+// const DATA = [
+//     {
+//       id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+//       title: 'First Item',
+//     },
+//     {
+//       id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+//       title: 'Second Item',
+//     },
+//     {
+//       id: '58694a0f-3da1-471f-bd96-145571e29d72',
+//       title: 'Third Item',
+//     },
+//   ];
+
+//   function Item({ title }) {
+//     return (
+//       <View style={styles.item}>
+//         <Text style={styles.title}>{title}</Text>
+//       </View>
+//     );
+//   }
+  
+// const CustomCard = () => {
+//     return (
+//         <View style={styles.container}>
+//             <FlatList
+//             style={{backgroundColor: 'green', flex: 1}}
+//             data={DATA}
+//             renderItem={({ item }) => <Item title={item.title} />}
+//             keyExtractor={item => item.id}>
+
+//             </FlatList>
+//         </View>
+//     )
+// }
+
+// export default CustomCard;
+
+// const styles = StyleSheet.create({
+//     container: {
+//         backgroundColor: CustomColor.lightGrayThemeColor, 
+//         height: 60, 
+//         flexDirection: 'row'
+//     },
+//     item: {
+//         backgroundColor: '#f9c2ff',
+//         padding: 20,
+//         marginVertical: 8,
+//         marginHorizontal: 16,
+//       },
+//     card: {
+//         backgroundColor: CustomColor.white, 
+//         flex: 0.7, 
+//         borderRadius: 5, 
+//         shadowColor: CustomColor.lightGray, 
+//         shadowOpacity: 1.0, 
+//         marginHorizontal: 16, 
+//         marginVertical: 5,
+//         shadowOffset: { width: 0, height: 0 },
+//     },
+//     title: {
+//         fontSize: 32,
+//       },
+// })
--- src/ViewControllers/Tabbar/Home.js
+++ src/ViewControllers/Tabbar/Home.js
@@ -1,9 +1,49 @@
+
 import React, { Component } from 'react';
-import { SectionList, View, StyleSheet, Text, SafeAreaView, Image, TouchableHighlight } from 'react-native';
+import { SectionList, View, StyleSheet, Text, SafeAreaView, Image, TouchableHighlight, FlatList } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import CustomColor from '../../Utility/Constant.js';//'../../Utility/Constant.js';
 import CustomCard from '../../Components/CustomCard.js';
+
+const sectionData = [
+    {
+      title: "Main dishes",
+      data: ["Pizza", "Burger", "Risotto"]
+    },
+    {
+      title: "Sides",
+      data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+    },
+    {
+      title: "Drinks",
+      data: ["Water", "Coke", "Beer"]
+    },
+    {
+      title: "Desserts",
+      data: ["Cheese Cake", "Ice Cream"]
+    }
+  ];
+
+//   const DATA = [
+//     {
+//       title: 'First Item',
+//     },
+//     {
+//       title: 'Second Item',
+//     },
+//     {
+//       title: 'Third Item',
+//     },
+//   ];
+  
+  function Item({ title }) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    );
+  }
 
 class Home extends Component {
 
@@ -25,20 +65,19 @@ class Home extends Component {
         return (
             <SafeAreaView>
 
-                <SectionList sections={[
-                    { title: 'Upcomming Classes', data: ['Success'] },
-                    { title: 'Recent Classes', data: ['Success'] },
-                ]}
+                <SectionList sections={sectionData}
                     renderItem={({ item }) => 
-                    <CustomCard></CustomCard>}
-
-                    renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-                    keyExtractor={(item, index) => index}
-                    ListHeaderComponent={this.liveClassView}
-                    stickyHeaderIndices={[0]}
-                >
-
-                </SectionList>
+                     <CustomCard></CustomCard>}></SectionList>
+                     {/* <FlatList
+                //       data={sectionData}
+                //       horizontal={true}
+                //       renderItem={({ item }) => <Item title={item.data} />}
+                //     />
+                // }
+                //      renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+                //      ListHeaderComponent={this.liveClassView}
+                // >
+                // </SectionList> */}
             </SafeAreaView>
         )
     }
@@ -67,11 +106,20 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: CustomColor.lightGrayThemeColor
     },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
+    // item: {
+    //     padding: 10,
+    //     fontSize: 18,
+    //     height: 44,
 
-    },
-
+    // },
+      item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        height: 100,
+      },
+      title: {
+        fontSize: 32,
+      },
 })
